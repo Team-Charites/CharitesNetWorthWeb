fcb6aa69f1559de4491e44df7401c4e899193a12
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-10-05 15:52:13 -0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Database Options
-$databaseHost = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
-$databaseName = 'xsv1qjd2he2pzjgh';
-$databaseUser = 'ze8g19ard2iom13t';
-$databasePassword = 'er7h7x0psn0ycdfa';
+$server = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+$user = 'xsv1qjd2he2pzjgh';
+$password = 'ze8g19ard2iom13t';
+$db = 'er7h7x0psn0ycdfa';
