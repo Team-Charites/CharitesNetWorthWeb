d3f9cc846259278d97a0ab3be0132bde07aba66e
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-10-06 00:22:05 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // header ('location:sign.php');
-$con = mysqli_connect('localhost','root');
+$con = mysqli_connect('bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306','ze8g19ard2iom13t');
 mysqli_select_db($con, 'userregistration');
 $fname = $_POST['firstname'];
 $lname = $_POST['lastname'];
